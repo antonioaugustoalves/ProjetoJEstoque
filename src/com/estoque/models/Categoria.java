90be1a6ac26a77e18a09bf73417e486b7d05848c
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2021-09-01 07:27:21 -0300

    Foram adicionadas as classes de Vendedor e de Categoria

--- src/com/estoque/models/Categoria.java
+++ src/com/estoque/models/Categoria.java
@@ -10,5 +10,37 @@ package com.estoque.models;
  * @author ANTONIO
  */
 public class Categoria {
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Categoria() {
+    }
+
+    @Override
+    public String toString() {
+        return this.id +")"+this.nome;
+    }
+    
     
 }
--- src/com/estoque/models/Vendedor.java
+++ src/com/estoque/models/Vendedor.java
@@ -10,5 +10,52 @@ package com.estoque.models;
  * @author ANTONIO
  */
 public class Vendedor {
+    private int id;
+    private String nome;
+    private String email;
+    private String celular;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Vendedor(int id, String nome, String email, String celular) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.celular = celular;
+    }
+
+    public Vendedor() {
+    }
+    
     
 }
